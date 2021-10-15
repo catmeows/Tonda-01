00e3928fea8d344f2cfcfb6e7dc08cbb61cf9a4b
catmeows <catmeows@users.noreply.github.com>
2021-10-15 22:09:54 +0200

    Update rom.asm
    clearVideo, setVideo

--- src/rom/rom.asm
+++ src/rom/rom.asm
@@ -1,7 +1,13 @@
 
 
+SYS_TEMP0     EQU $BF00
+SYS_TEMP1     EQU $BF01
+SYS_TEMP2     EQU $BF02
+SYS_TEMP3     EQU $BF03
 
-SYS_VAR_TIMER EQU $BFB0
+SYS_GATTR     EQU $BFAE
+SYS_MARTAREG  EQU $BFAF
+SYS_TIMER     EQU $BFB0
 
 SWI3_VECTOR   EQU $BFB4
 SWI2_VECTOR   EQU $BFB6
@@ -12,12 +18,91 @@ NMI_VECTOR    EQU $BFBE
 
 MARTA_IO      EQU $BFC0
 
-SET_VIDEO
-     ;A video mode
+
+
+clearVideo
+     ;clear screen
+     LDA >SYS_MARTAREG
+     ANDA #$03
+     BEQ clearVideo0 
+     DECA
+     BNE clearVideo2
+     ;clear video 1
+     LDA >SYS_GATTR				;get global paper
+     ANDA #$F0
+     STA >SYS_TEMP0
+     LSRA
+     LSRA
+     LSRA
+     LSRA
+     ORA >SYS_TEMP0                             ;construct fill byte 
+     LDY #$1800
+     BRA clearVideoA				;fill $1800 bytes by PAPER*16+PAPER
+clearVideo2
+     DECA
+     BNE clearVideo3
+     ;clear video 0,2
+clearVideo0     
+     CLRA                                        ;clear $1800 bytes from $0000
+     LDY #$1800
+     BSR clearVideoA
+     LDA >SYS_GATTR				 ;clear $300 bytes by PAPER*16+INK
+     LDX $1800
+     LDY $300
+     JMP fastFill
+clearVideo3
+     ;clear video mode 3
+     LDA >SYS_MARTAREG
+     ORA #$08                                    ;page shadow ram in
      CLRB
-     STB 
-     LDB #$FC
+     JSR setMartaRegister
+     LDY #$1E00
+     JSR clearVideo9                             ;clear second half of screen by PAPER*64+PAPER*16+PAPER*4+PAPER
+     LDA >SYS_MARTAREG
+     AND #$F3                                    ;page base ram in and set video page 
+     STA >SYS_MARTAREG
+     CLRB
+     JSR setMartaRegister
+     LDY #$1E00                                  ;clear first half of screen by PAPER*64+PAPER*16+PAPER*4+PAPER
+clearVideo9
+     LDA >SYS_GATTR                              ;get global paper
+     AND #$30
+     LSLA
+     LSLA
+     STA >SYS_TEMP0
+     LDB #$03
+clearVideo9a
+     LSRA >SYS_TEMP0
+     LSRA >SYS_TEMP0
+     ORA >SYS_TEMP0                              ;construct fill byte            
+     DECB
+     BNE clearVideo9a
+clearVideoA     
+     LDX #$0000                                  ;start fill from $0000
+     JMP fastFill
      
+
+setVideo
+     ;A video mode
+     ;write to Marta control register
+     LDB >SYS_MARTAREG
+     ANDB #$FC
+     STB >SYS_MARTAREG
+     ANDA #$03
+     ORA >SYS_MARTAREG
+     STA >SYS_MARTAREG
+     CLRB
+     JSR setMartaRegister
+     RTS
+  
+setMartaRegister
+     ;B register to set
+     ;A value
+     ANDB #$1F
+     STB >$BFC0
+     ORA #$80
+     STA >$BFC0
+     RTS
      
      
 SET_BITS
@@ -89,17 +174,17 @@ fastCopyExit
         PULS U
         RTS           
 
-SHORT_COPY
+shortCopy
      ;X destination
      ;Y source
      ;B counter
            LDA ,Y+
 	   STA ,X+
 	   DECB
-	   BNE SHORT_COPY
+	   BNE shortCopy
 	   RTS
 
-FAST_FILL
+fastFill
     ;A fill byte
     ;X destination
     ;Y counter
@@ -107,78 +192,80 @@ FAST_FILL
 	  EXG D,Y
 	  BITB #$01
 	  EXG D,Y
-	  BEQ FILL_EVEN
+	  BEQ fillEven
 	  STA ,X+
 	  LEAY -1,Y
-	  BEQ FILL_EXIT
-FILL_EVEN
+	  BEQ fillExit
+fillEven
 	  TFR A,B
-FILL_LOOP
+fillLoop
 	  STD ,X++
 	  LEAY -2,Y
-	  BNE FILL_LOOP    ;3   -> 8clocks/byte
-FILL_EXIT
+	  BNE fillLoop                         ;3   -> 8clocks/byte
+fillExit
 	  RTS
 	  
-RESET_HANDLER
-	  LDD #RESET_HANDLER
+resetHandler
+	  LDD #resetHandler
 	  STD NMI_VECTOR                       ;set default NMI handler to RESET
 	  LDS #$BDFE                           ;set stack below system variables
 	  LDX #SWI3_VECTOR		       ;vectors in ram
-	  LDY #DEFAULT_VECTORS                 ;default vectors
+	  LDY #defaultVectors                 ;default vectors
 	  LDB #10                              ;5x2 bytes
-	  JSR SHORT_COPY                       ;copy default to vectors
+	  JSR shortCopy                        ;copy default to vectors
 	  LDD #$0000		               ;reset timer
-          STD SYS_VAR_TIMER
-          STD SYS_VAR_TIMER+2
+          STD SYS_TIMER
+          STD SYS_TIMER+2
           LDA #$BF                             ;set direct page 
           TFR A,DP
-
-DEFAULT_IRQ_HANDLER
-          LDD SYS_VAR_TIMER+2 		       ;increase four byte timer
+	  CLRA
+	  JSR setVideo				;set graphics mode 0
+	  
+defaultIrqHandler
+          LDD SYS_TIMER+2 		       ;increase four byte timer
           ADDD #$0001
-          STD SYS_VAR_TIMER+2
-          BCC DEFAULT_IRQ_HANDLER1
-          LDX SYS_VAR_TIMER
+          STD SYS_TIMER+2
+          BCC defaultIrqHandler1
+          LDX SYS_TIMER
           LEAX +1,X 
-          STX SYS_VAR_TIMER
-DEFAULT_IRQ_HANDLER1    
-          JSR SYS_SCAN_KEYS 		       ;scan keyboard
-DEFAULT_HANDLER
-          RTI  			               ;simply do nothing and leave
+          STX SYS_TIMER
+defaultIrqHandler1    
+          JSR SYS_SCAN_KEYS 		            ;scan keyboard
+defaultHandler
+          RTI  			                    ;simply do nothing and leave
 
-DEFAULT_VECTORS
-         FDB DEFAULT_HANDLER                        ;default handler for SWI3
-         FDB DEFAULT_HANDLER                        ;default handler for SWI2
-         FDB DEFAULT_HANDLER                        ;default handler for SWI
-         FDB DEFAULT_HANDLER                        ;default handler for FIRQ
-         FDB DEFAULT_IRQ_HANDLER                    ;default handler for IRQ
+defaultVectors
+         FDB defaultHandler                         ;default handler for SWI3
+         FDB defaultHandler                         ;default handler for SWI2
+         FDB defaultHandler                         ;default handler for SWI
+         FDB defaultHandler                         ;default handler for FIRQ
+         FDB defaultIrqHandler                      ;default handler for IRQ
 
-NMI_HANDLER
-    JMP [NMI_VECTOR]		              ;jump vector from NMI_VECTOR 
+nmiHandler
+    JMP [NMI_VECTOR]		                    ;jump vector from NMI_VECTOR 
 
-IRQ_HANDLER
-    JMP [IRQ_VECTOR]		              ;jump vector from IRQ_VECTOR 
+irqHandler
+    JMP [IRQ_VECTOR]		                    ;jump vector from IRQ_VECTOR 
 
-FIRQ_HANDLER
-    JMP [FIRQ_VECTOR]		              ;jump vector from FIRQ_VECTOR 
+firqHandler
+    JMP [FIRQ_VECTOR]		                    ;jump vector from FIRQ_VECTOR 
 
-SWI_HANDLER
-    JMP [SWI_VECTOR]		              ;jump vector from SWI_VECTOR 
+swiHandler
+    JMP [SWI_VECTOR]		                    ;jump vector from SWI_VECTOR 
 
-SWI2_HANDLER
-    JMP [SWI2_VECTOR]		              ;jump vector from SWI2_VECTOR 
+swi2Handler
+    JMP [SWI2_VECTOR]		                    ;jump vector from SWI2_VECTOR 
 
-SWI3_HANDLER
-    JMP [SWI3_VECTOR]		              ;jump vector from SWI3_VECTOR 
+swi3Handler
+    JMP [SWI3_VECTOR]		                    ;jump vector from SWI3_VECTOR 
 
     ORG $FFF0
 
     FDB $FFFF			                    ;reserved
-    FDB SWI3_HANDLER 		              ;swi3 handler
-    FDB SWI2_HANDLER		              ;swi2 handler
-    FDB FIRQ_HANDLER		              ;firq handler
-    FDB IRQ_HANDLER		                ;irq handler
-    FDB SWI_HANDLER 		              ;swi handler
-    FDB NMI_HANDLER		                ;nmi handler
-    FDB RESET_HANDLER		              ;reset handler
+    FDB SWI3_HANDLER 		                    ;swi3 handler
+    FDB SWI2_HANDLER		                    ;swi2 handler
+    FDB FIRQ_HANDLER		                    ;firq handler
+    FDB IRQ_HANDLER		                    ;irq handler
+    FDB SWI_HANDLER 		                    ;swi handler
+    FDB NMI_HANDLER		                    ;nmi handler
+    FDB RESET_HANDLER		                    ;reset handler
