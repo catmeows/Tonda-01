f0c79187950a46783555cfa47f1db1753da65ffa
catmeows <catmeows@users.noreply.github.com>
2021-11-22 23:31:19 +0100

    Create sysrom-printchar.asm

---
+++ src/rom/sysrom-printchar.asm
@@ -0,0 +1,46 @@
+
+
+
+    LDB #$08                      ;8 bytes of character
+    PSHS Y
+printCharMode1c    
+    LDA ,X+
+    TST <SYS_INVERSE              ;inverse bits?
+    BPL printCharMode1a
+    COMA
+printCharMode1a
+    STA ,Y
+    LEAY 32,Y
+    TST <SYS_CSIZE                ;double heigth?
+    BPL printCharMode1b
+    STA ,Y
+    LEAY 32,Y
+printCharMode1b
+    DECB
+    BNE printCharMode1c
+    PULS Y
+    JSR yPix2yAttr
+    LDA <SYS_TEMPATTR
+    STA ,Y
+    TST <SYS_CSIZE
+    BPL printCharMode1d
+    STA 32,Y
+printCharMode1d
+    RTS
+    
+getCharPattern
+    CMPA #$20
+    BCC getCharPattern2
+    LDX SYS_UDG
+getCharPattern2
+    LDX SYS_FONT
+getCharPattern1
+    LDB #$08
+    MUL
+    LEAX D,X
+    RTS
+    
+    
+    
+    
+    
