6a5d17beb8a6abb9fa86e6f6b6227f27c1aa23a1
catmeows <catmeows@users.noreply.github.com>
2022-10-16 22:19:01 +0200

    Create memManagement.asm

---
+++ src/rom/memManagement.asm
@@ -0,0 +1,41 @@
+; MEMORY MANAGEMENT
+; BASIC uses 
+;
+; Program area is storage for tokenized BASIC lines. Whenever a line is added, removed or changes, program area size changes.
+; It is expected that program area does not change during execution of program.
+;
+; Numeric array area is storage for numeric arrays. Whenever new numeric array is created using DIM command, space for the array
+; is created in numeric array area. Commands CLEAR, NEW erase all variables and therefore colapse numeric array area.
+;
+; Numeric variables area is storage for numeric variables. When a new numeric variable is created, space for variable is created
+; for the variable. Commands CLEAR, NEW erase all variables and therefore colapse numeric array area.
+;
+; Labels are is storage for labels in program. Whenever commands GOTO, GOSUB, FN, RESTORE are looking for a label, they will check
+; label area to quickly get address of label. If label is unknown, program is searched and if label is found, it is inserted into 
+; label area. All labels are discarded and Label area is collapsed when BASIC leaves execution mode and enters edit mode.
+;
+; String variable area is storage for both of single strings and string arrays. When a string variable is created or changes its length,
+; size of string variable area is adjusted. Commands CLEAR, NEW erase all variables and therefore colapses numeric array area.
+;
+; Loop area is storage for active loops FOR and WHILE and return pointers for GOSUB and FN. This area is collapsed by CLEAR, NEW, RUN.
+;
+; Edit line area is storage for edited line. It is used by program editor and by INPUT command. For most time, this area is collapsed.
+;
+; Workspace is temporary area used during evaluating expressions. It is also used as storage for tokenized program line.
+;
+; 6809's U stack is used as calculator stack.
+;
+; 6809's S stack is used as machine code stack.
+;
+; PROGS start of program area
+; NUMARRAYS start of numeric array area
+; NUMVARS start of numeric variables area
+; LABELS start of label area
+; STRINGS start of string variable area 
+; LOOPS start of loop area
+; ELINE start of edit line area
+; WRKSPC is start of workspace area
+; FREEMEM is start of unused memory available to BASIC
+; RAMTOP is start of memery area that is not available to BASIC
+
+
