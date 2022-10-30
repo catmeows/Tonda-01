429d0cb3274e4bf5ec6db86565f6e495e17589be
catmeows <catmeows@users.noreply.github.com>
2022-10-30 23:06:54 +0100

    Update systemVariables.asm

--- src/rom/systemVariables.asm
+++ src/rom/systemVariables.asm
@@ -12,7 +12,25 @@ FREEMEM       EQU $BE10
 INSERTPTR     EQU $BE12
 RESERVE       EQU $BE14
 
+
+TEMP0         EQU $BEF0 
+TEMP1         EQU $BEF1
+TEMP2         EQU $BEF2
+TEMP3         EQU $BEF3
+TEMP4         EQU $BEF4
+TEMP5         EQU $BEF5
+TEMP6         EQU $BEF6
+TEMP7         EQU $BEF7
+TEMP8         EQU $BEF8
+TEMP9         EQU $BEF9
+TEMPA         EQU $BEFA
+TEMPB         EQU $BEFB
+TEMPC         EQU $BEFC
+TEMPD         EQU $BEFD
+TEMPE         EQU $BEFE
+TEMPF         EQU $BEFF
+
 ;other variables
 
 TIME          EQU $BF00
-
+TSTACK        EQU $BF04
