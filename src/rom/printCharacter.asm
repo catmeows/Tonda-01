72c245bf1ffaef1e58e7e52c2170f00863c18116
catmeows <catmeows@users.noreply.github.com>
2022-11-03 20:49:43 +0100

    Update printCharacter.asm

--- src/rom/printCharacter.asm
+++ src/rom/printCharacter.asm
@@ -50,8 +50,43 @@ printChar1
   LDB #$08                    ;character pattern is 8 bytes long
   MUL
   LEAX D, X                   ;now we have pointer to pattern
-  LDA <PRTPOS_LINE            ;get current print line
+  LDA <PRTPOS_LINE            ;get current print line into A
+  LDB #$A0                    ;320/2=160
   TST <GMODE                  ;check current graphics mode
+  BEQ printPosM0              ;GMODE==0 it is mode 0
+  BPL printPosM1              ;GMODE==1 it is mode 1, GMODE==128 it is mode 2
+  
+  ;compute position for mode 2
+  ASLA
+  ASLA                        ;multiply line by 4
+  MUL                         ;line*4*160=line*640
+  STD <TEMP0                  ;store start of line to TEMP0, TEMP1
+  LDB <PRTPOS_COL             ;get print column
+  ASLB
+  ASLB                        ;column*4, since column is in range 0..19, it will never overflow
+  CLRA                        ;clear MSB of D
+  ADDD <TEMP0                 ;line*640+column*4, we have ptr to screen memory now
+  BRA printPosDone            ;continue to common part of print routine
+  
+printPosM0
+  ;compute position for mode 0
+  ASLA                        ;multiply line by 2
+  MUL                         ;line*2*160=line*320
+  ADDB <PRTPOS_COL            ;add column
+  ADCA #$00                   ;update MSB of ptr
+  BRA printPosDone
+  
+printPosM1
+  ;compute position for mode 1
+  ASLA
+  ASLA                        ;multiply line by 4
+  MUL                         ;line*4*160=line*640
+  ADDB <PRTPOS_COL
+  ADCA #$00
+  ADDB <PRTPOS_COL
+  ADCA #$00                   ;line*640+column*2
+printPosDone
+  TFR D, Y                    ;store pointer to screen into Y
   
 
 
