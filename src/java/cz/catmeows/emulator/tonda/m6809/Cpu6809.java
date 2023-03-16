555cb21e14ceb702c8b66244ac4cb494546d5215
catmeows <catmeows@users.noreply.github.com>
2023-03-16 19:27:35 +0100

    Add LBRA, LBSR, DAA, ORCC, ANDCC, SEX, EXG, TFR, BRA, BRN, BHI, BLS, BCC, BCS, BNE, BVC, BVS, BMI, BGE, BLT, BGT, BLE.

--- src/java/cz/catmeows/emulator/tonda/m6809/Cpu6809.java
+++ src/java/cz/catmeows/emulator/tonda/m6809/Cpu6809.java
@@ -25,6 +25,10 @@ public class Cpu6809 {
     private int regPC;
     private int regCC;
 
+    private boolean lineNMI;
+    private boolean lineIRQ;
+    private boolean lineFIRQ;
+
     private AddressSpace mem;
     private TickListener tickListener;
 
@@ -161,6 +165,18 @@ public class Cpu6809 {
 
     public void setCCOverflow(boolean ccOverflow) {
         setCCFlag(CC_OVER, ccOverflow);
+    }
+
+    public void setLineNMI(boolean nmi) {
+        lineNMI = nmi;
+    }
+
+    public void setLineIRQ(boolean irq) {
+        lineIRQ = irq;
+    }
+
+    public void setLineFIRQ(boolean firq) {
+        lineFIRQ = firq;
     }
 
     private void setCCFlag(int flagBit, boolean bit) {
@@ -308,7 +324,145 @@ public class Cpu6809 {
                 tickListener.tick();
                 break;
             case 0x10:
-                //
+                //Page 2
+                //TODO Implement page 2 opcodes
+                break;
+            case 0x11:
+                //Page 3
+                //TODO Implement page 3 opcodes
+                break;
+            case 0x12:
+                //NOP, 2
+                tickListener.tick();
+                break;
+            case 0x13:
+                //SYNC, 4+
+                tickListener.tick();
+                do {
+                    tickListener.tick();
+                } while (!(lineNMI||lineIRQ||lineFIRQ));
+                tickListener.tick();
+                break;
+            case 0x14:
+                //illegal
+                break;
+            case 0x15:
+                //illegal
+                break;
+            case 0x16:
+                //LBRA, 5
+                helperLongBranch(true);
+                break;
+            case 0x17:
+                //LBSR, 9
+                helperLongSubroutine();
+                break;
+            case 0x18:
+                //illegal
+                break;
+            case 0x19:
+                //DAA, 2
+                helperDaa();
+                break;
+            case 0x1A:
+                //ORCC immediate, 3
+                setCCReg((getImmediate()|getCCReg())&0xff);
+                tickListener.tick();
+                break;
+            case 0x1B:
+                //illegal
+                break;
+            case 0x1C:
+                //ANDCC immediate, 3
+                setCCReg((getImmediate()&getCCReg())&0xff);
+                tickListener.tick();
+                break;
+            case 0x1D:
+                //SEX, 2
+                regA = ((regB&0x80)==0x80)?0xff:0x00;
+                setCCNegative((regB&0x80)==0x80);
+                setCCZero(regB==0);
+                tickListener.tick();
+                break;
+            case 0x1E:
+                //EXG postbyte, 8
+                helperExg(getImmediate());
+                break;
+            case 0x1F:
+                //TFR postbyte, 6
+                helperTfr(getImmediate());
+                break;
+            case 0x20:
+                //BRA offset, 3
+                helperBranch(true);
+                break;
+            case 0x21:
+                //BRN offset, 3
+                helperBranch(false);
+                break;
+            case 0x22:
+                //BHI offset, 3
+                helperBranch(!(getCCCarry()||getCCZero()));
+                break;
+            case 0x23:
+                //BLS offset, 3
+                helperBranch(getCCCarry()||getCCZero());
+                break;
+            case 0x24:
+                //BCC offset, 3
+                helperBranch(!getCCCarry());
+                break;
+            case 0x25:
+                //BCS offset, 3
+                helperBranch(getCCCarry());
+                break;
+            case 0x26:
+                //BNE offset, 3
+                helperBranch(!getCCZero());
+                break;
+            case 0x27:
+                //BEQ offset, 3
+                helperBranch(getCCZero());
+                break;
+            case 0x28:
+                //BVC offset, 3
+                helperBranch(!getCCOverflow());
+                break;
+            case 0x29:
+                //BVS offset, 3
+                helperBranch(getCCOverflow());
+                break;
+            case 0x2A:
+                //BPL offset, 3
+                helperBranch(!getCCNegative());
+                break;
+            case 0x2B:
+                //BMI offset, 3
+                helperBranch(getCCNegative());
+                break;
+            case 0x2C:
+                //BGE offset, 3
+                helperBranch(getCCNegative()==getCCOverflow());
+                break;
+            case 0x2D:
+                //BLT offset, 3
+                helperBranch(getCCNegative()!=getCCOverflow());
+                break;
+            case 0x2E:
+                //BGT offset, 3
+                helperBranch((!getCCZero())&&(getCCNegative()==getCCOverflow()));
+                break;
+            case 0x2F:
+                //BLE offset, 3
+                helperBranch(getCCZero()||(getCCNegative()!=getCCOverflow()));
+                break;
+            case 0x30:
+                //LEAX ,4+
+
+            case 0x31:
+                //LEAY ,4+
+
+
         }
 
 
@@ -323,6 +477,23 @@ public class Cpu6809 {
         incPC();
         tickListener.tick();
         return (regDP<<8)+lowEA;
+    }
+
+    private int getImmediate() {
+        int value = mem.read(regPC)&0xff;
+        incPC();
+        tickListener.tick();
+        return value;
+    }
+
+    private int getImmediateWord() {
+        int ofsHi = mem.read(regPC);
+        incPC();
+        tickListener.tick();
+        int ofsLo = mem.read(regPC);
+        incPC();
+        tickListener.tick();
+        return ((ofsHi<<8)+ofsLo)&0xffff;
     }
 
     private int helperNeg(int value) {
@@ -413,4 +584,339 @@ public class Cpu6809 {
         setCCCarry(false);
         return 0;
     }
+
+    private void helperBranch(boolean condition) {
+        int ofsLo = mem.read(regPC);
+        incPC();
+        tickListener.tick();
+        int ofsHi = ((ofsLo&0x80)==0x80)?0xff:0x00;
+        tickListener.tick();
+        if (condition) {
+            setPCReg((regPC+((ofsHi<<8)+ofsLo))&0xffff);
+        }
+    }
+
+    private void helperLongBranch(boolean condition) {
+        int ofsHi = mem.read(regPC);
+        incPC();
+        tickListener.tick();
+        int ofsLo = mem.read(regPC);
+        incPC();
+        tickListener.tick();
+        tickListener.tick();
+        if (condition) {
+            setPCReg((regPC+((ofsHi<<8)+ofsLo))&0xffff);
+            tickListener.tick();
+        }
+    }
+
+    private void helperLongSubroutine() {
+        int ofsHi = mem.read(regPC);
+        incPC();
+        tickListener.tick();
+        int ofsLo = mem.read(regPC);
+        incPC();
+        tickListener.tick();
+        tickListener.tick();
+        tickListener.tick();
+        int ea = (regPC+((ofsHi<<8)+ofsLo))&0xffff;
+        mem.read(ea);
+        tickListener.tick();
+        tickListener.tick();
+        regS=(regS-1)&0xffff;
+        mem.write(regS, regPC&0xff);
+        tickListener.tick();
+        regS=(regS-1)&0xffff;
+        mem.write(regS, (regPC&0xff00)>>8);
+        regPC = ea;
+        tickListener.tick();
+    }
+
+    private void helperDaa() {
+        int lowNibble = regA&0x0f;
+        int hiNibble = (regA&0xf0)>>4;
+        /*  So now we are after ADDA or ADCA instruction
+            for lower nibble, either decimal overflowed to hexadecimal e.g. 6+6=0xC
+                              or it overflowed to next nibble e.g. 9+9=0x12 (half-carry flag is set)
+                              if so, we need to adjust lower nibble
+        */
+        int loCorrection = ((lowNibble>9)|getCCHalfCarry())?0x06:00;
+        /*
+            for higher nibble, decimal overflowed to hexadecimal
+                               or result overflowed to next byte
+                               or the lower nibble overflowed to hexadecimal and higher nibble is 9 or greater:
+                                  if so, loCorrection will add 6 to low nibble, causing increase in hi nibble too
+                                  and moving hi nibble outside of decimal range
+                               note, if half-carry is set then higher nibble was increased by one during ADDA/ADCA
+                               already
+        */
+        int hiCorrection = (getCCCarry() || (hiNibble>9) || ((hiNibble>8)&&(lowNibble>9))) ? 0x60:0x00;
+        int result = regA + (loCorrection | hiCorrection);
+        /*
+            set carry if there is overflow to next byte, the carry flag should be preserved for following ADCA
+            if there was carry after last ADCA/ADDA e.g. 0x90+0x90=0x120, we need it to preserve for following ADCA
+        */
+        setCCCarry(getCCCarry() || ((result&0x100) == 0x100));
+        setCCZero((result&0xff)==0);
+        setCCNegative((result&0x80)==0x80);
+        regA = result;
+        tickListener.tick();
+    }
+
+    private void helperExg(int postByte) {
+        int r1 = postByte&0xf;
+        int r2 = (postByte>>4)&0xf;
+        int r1Value = getRegByPostByte(r1);
+        int r2Value = getRegByPostByte(r2);
+        setRegByPostByte(r1, r2Value);
+        setRegByPostByte(r2, r1Value);
+        tickListener.tick();
+        tickListener.tick();
+        tickListener.tick();
+        tickListener.tick();
+        tickListener.tick();
+        tickListener.tick();
+    }
+
+    private void helperTfr(int postByte) {
+        setRegByPostByte((postByte>>4)&0xf, getRegByPostByte(postByte&0xf));
+        tickListener.tick();
+        tickListener.tick();
+        tickListener.tick();
+        tickListener.tick();
+    }
+
+    private void setRegByPostByte(int code, int value) {
+        switch (code) {
+            case 0x0:
+                setDReg(value);
+                break;
+            case 0x1:
+                regX = value&0xffff;
+                break;
+            case 0x2:
+                regY = value&0xffff;
+                break;
+            case 0x3:
+                regU = value&0xffff;
+                break;
+            case 0x4:
+                regS = value&0xffff;
+                break;
+            case 0x5:
+                regPC = value&0xffff;
+                break;
+            case 0x8:
+                regA = value&0xff;
+                break;
+            case 0x9:
+                regB = value&0xff;
+                break;
+            case 0xA:
+                regCC = value&0xff;
+                break;
+            case 0xB:
+                regDP = value&0xff;
+                break;
+        }
+    }
+
+    private int getRegByPostByte(int code) {
+        switch (code) {
+            case 0x0:
+                return getDReg();
+            case 0x1:
+                return regX;
+            case 0x2:
+                return regY;
+            case 0x3:
+                return regU;
+            case 0x4:
+                return regS;
+            case 0x5:
+                return regPC;
+            case 0x8:
+                return 0xff00+regA;
+            case 0x9:
+                return 0xff00+regB;
+            case 0xA:
+                return 0xff00+regCC;
+            case 0xB:
+                return 0xff00+regDP;
+        }
+        return 0xffff;
+    }
+
+    private int indexedEa() {
+        int postByte = getImmediate();
+        if ((postByte&0x80)==0) {
+            int ea = indexedGetReg(postByte);
+            return (ea + ((postByte&0x1f)|(((postByte&0x10)==0x10)?0xffe0:0x0000)))&0xffff;
+        }
+        if (postByte==0x9f) {
+            int addr = getImmediateWord();
+            tickListener.tick();
+            return getIndirectEa(addr);
+        }
+        int ea=-1;
+        switch (postByte&0x0f) {
+            case 0x0:
+                //R+
+                ea = indexedGetReg(postByte);
+                indexedRegPlus(postByte);
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                break;
+            case 0x1:
+                //R++
+                ea = indexedGetReg(postByte);
+                indexedRegPlus(postByte);
+                indexedRegPlus(postByte);
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                break;
+            case 0x2:
+                //-R
+                indexedRegMinus(postByte);
+                ea = indexedGetReg(postByte);
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                break;
+            case 0x3:
+                //--R
+                indexedRegMinus(postByte);
+                indexedRegMinus(postByte);
+                ea = indexedGetReg(postByte);
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                break;
+            case 0x4:
+                //,R
+                ea = indexedGetReg(postByte);
+                tickListener.tick();
+                break;
+            case 0x5:
+                //B,R
+                ea = addExtended8BitTo16Bit(indexedGetReg(postByte),regB);
+                tickListener.tick();
+                tickListener.tick();
+                break;
+            case 0x6:
+                //A,R
+                ea = addExtended8BitTo16Bit(indexedGetReg(postByte),regA);
+                tickListener.tick();
+                tickListener.tick();
+                break;
+            case 0x8:
+                //n,R
+                ea = addExtended8BitTo16Bit(indexedGetReg(postByte),getImmediate());
+                tickListener.tick();
+                break;
+            case 0x9:
+                //nn,R
+                ea = add16BitTo16Bit(indexedGetReg(postByte),getImmediateWord());
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                break;
+            case 0xB:
+                //D,R
+                ea = add16BitTo16Bit(indexedGetReg(postByte), getDReg());
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                break;
+            case 0xC:
+                //n,PC
+                int ofs = getImmediate();
+                ea = addExtended8BitTo16Bit(regPC, ofs);
+                tickListener.tick();
+                break;
+            case 0xD:
+                //nn,PC
+                ofs = getImmediateWord();
+                ea = add16BitTo16Bit(regPC, ofs);
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                tickListener.tick();
+                break;
+        }
+        if ((postByte&0x10)==0x10) {
+            ea = getIndirectEa(ea);
+        }
+        return ea;
+    }
+
+    private int getIndirectEa(int addr) {
+        int hi = mem.read(addr);
+        tickListener.tick();
+        int lo = mem.read((addr+1)&0xffff);
+        tickListener.tick();
+        tickListener.tick();
+        return ((hi<<8)+lo)&0xffff;
+    }
+
+    private int indexedGetReg(int postByte) {
+        switch (postByte&0x60) {
+            case 0x00:
+                return regX;
+            case 0x20:
+                return regY;
+            case 0x40:
+                return regU;
+        }
+        return regS;
+    }
+
+    private void indexedRegMinus(int postByte) {
+        switch (postByte&0x60) {
+            case 0x00:
+                regX = (regX - 1)&0xffff;
+                break;
+            case 0x20:
+                regY = (regY - 1)&0xffff;
+                break;
+            case 0x40:
+                regU = (regU - 1)&0xffff;
+                break;
+            case 0x60:
+                regS = (regS - 1)&0xffff;
+                break;
+        }
+    }
+
+    private void indexedRegPlus(int postByte) {
+        switch (postByte&0x60) {
+            case 0x00:
+                regX = (regX + 1)&0xffff;
+                break;
+            case 0x20:
+                regY = (regY + 1)&0xffff;
+                break;
+            case 0x40:
+                regU = (regU + 1)&0xffff;
+                break;
+            case 0x60:
+                regS = (regS + 1)&0xffff;
+                break;
+        }
+    }
+
+    private int addExtended8BitTo16Bit(int aWord, int aByte) {
+        return (aWord + ((aByte&0x80)==0x80?0xffff:0x0000))&0xffff;
+    }
+
+    private int add16BitTo16Bit(int aWord, int anotherWord) {
+        return (aWord+anotherWord)&0xffff;
+    }
+
 }
